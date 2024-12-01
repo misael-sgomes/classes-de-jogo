6acfbf76735e9b25669ff371df417cf63d869c28
misael-sgomes <misaelgomes88@gmail.com>
2024-12-01 17:32:20 -0300

    codigo class

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-class ()
+class Hero {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        if (this.tipo === "Guerreiro") {
+            console.log(`${this.tipo} atacou usando Espada`);
+        } else if (this.tipo === "Mago") {
+            console.log(`${this.tipo} atacou usando Magia`);
+        }
+    }
+}
+
+let heroOne = new Hero("Stark", "50", "Guerreiro");
+let heroTwo = new Hero("Thor", "300", "Mago");
+
+heroOne.atacar();
+heroTwo.atacar();
